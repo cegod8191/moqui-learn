9f922604432994acbb65ab97141d5dad7430e519
nbzx <zjnbzx@gmail.com>
2012-10-28 22:43:43 +0800

    用户账户管理实现所属组的更改后保存功能

--- runtime/component/usermanage/script/usermanage/user/updateUserMemberGroup.groovy
+++ runtime/component/usermanage/script/usermanage/user/updateUserMemberGroup.groovy
@@ -1,4 +1,6 @@
 import org.apache.log4j.Logger
 logger = Logger.getLogger("updateUserMemberGroup")
 
+logger.info("-------------------------- create")
+
 logger.info("=========== updateUserMemberGroup: ${context}")
