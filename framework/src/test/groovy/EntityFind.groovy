0843d1bc05c2d5792d9f7014629be026e2f86c5e
Paul Z. X <zjnbzx@gmail.com>
2012-11-17 11:06:58 +0800

    Added copyright at entity find test case.

--- framework/src/test/groovy/EntityFind.groovy
+++ framework/src/test/groovy/EntityFind.groovy
@@ -1,4 +1,16 @@
-import spock.lang.*
+/*
+ * This Work is in the public domain and is provided on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied,
+ * including, without limitation, any warranties or conditions of TITLE,
+ * NON-INFRINGEMENT, MERCHANTABILITY, or FITNESS FOR A PARTICULAR PURPOSE.
+ * You are solely responsible for determining the appropriateness of using
+ * this Work and assume any risks associated with your use of this Work.
+ *
+ * This Work includes contributions authored by Paul Z. Xu, not as a
+ * "work for hire", who hereby disclaims any copyright to the same.
+ */
+
+ import spock.lang.*
 
 import org.moqui.context.ExecutionContext
 import org.moqui.entity.EntityValue
@@ -16,7 +28,7 @@ class EntityFind extends Specification {
     def setupSpec() {
         // init the framework, get the ec
         ec = Moqui.getExecutionContext()
-        timestamp = new Timestamp(System.currentTimeMillis())//.valueOf("2012-11-13 10:21:32")
+        timestamp = new Timestamp(System.currentTimeMillis())
     }
 
     def cleanupSpec() {
